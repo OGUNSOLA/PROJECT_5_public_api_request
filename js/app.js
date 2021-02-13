180b99cc79a73d84a601c57ab6362b8dc3a73bc8
ogunsola <olubukola@me.com>
2021-02-13 10:02:48 -0500

    .

--- js/app.js
+++ js/app.js
@@ -24,22 +24,60 @@ function fetchData(url){
 
 function generateHTML (people){
     let eachPerson;
-    people.forEach(person =>{
+
+    for(let i=people.length-1; i>=0; i--){
         eachPerson = `<div class="card">
         <div class="card-img-container">
-            <img class="card-img" src=${person.picture.thumbnail} alt="profile picture">
+            <img class="card-img" src=${people[i].picture.thumbnail} alt="profile picture">
         </div>
         <div class="card-info-container">
-            <h3 id="name" class="card-name cap">${person.name.first} ${person.name.last}</h3>
-            <p class="card-text">${person.email}</p>
-            <p class="card-text cap">${person.location.city}, ${person.location.state}</p>
+            <h3 id="name" class="card-name cap">${people[i].name.first} ${people[i].name.last}</h3>
+            <p class="card-text">${people[i].email}</p>
+            <p class="card-text cap">${people[i].location.city}, ${people[i].location.state}</p>
         </div>
         </div>`;
            galley.insertAdjacentHTML('afterbegin', eachPerson);
-        
-    });
+    }
+    
 }
 
-const card = document.querySelectorAll('.card');
-console.log(card)
+const cards = document.querySelectorAll('.card');
+console.log(cards);
 
+galley.addEventListener('click', (e)=>{
+    const cards = document.querySelectorAll('.card');
+
+    for(let i=0;i< cards.length; i++){
+        if(e.target === cards[i]){
+            createModal(i);
+        }
+    }
+   
+})
+
+function createModal(i){
+    
+    const overlay = `<div class="modal-container">
+                        <div class="modal">
+                            <button type="button" id="modal-close-btn" class="modal-close-btn"><strong>X</strong></button>
+                            <div class="modal-info-container">
+                                <img class="modal-img" src="${peopleArray[i].picture.large}" alt="picture of ${peopleArray[i].name}">
+                                <h3 id="name" class="modal-name cap">${peopleArray[i].name.first} ${peopleArray[i].name.last}</h3>
+                                <p class="modal-text">${peopleArray[i].email}</p>
+                                <p class="modal-text cap">${peopleArray[i].location.city}</p>
+                                <hr>
+                                <p class="modal-text">${peopleArray[i].cell}</p>
+                                <p class="modal-text">123 Portland Ave., Portland, OR 97204</p>
+                                <p class="modal-text">Birthday: 10/21/2015</p>
+                            </div>
+                        </div>
+
+                        // IMPORTANT: Below is only for exceeds tasks 
+                        <div class="modal-btn-container">
+                            <button type="button" id="modal-prev" class="modal-prev btn">Prev</button>
+                            <button type="button" id="modal-next" class="modal-next btn">Next</button>
+                        </div>
+                    </div>`;
+
+                body.insertAdjacentHTML('beforeend', overlay);
+ }
